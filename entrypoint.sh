941f4ed9a7cb0e810ffe0a4272b6afbf11e8fb51
gauthamgirish50 <gauthamgirish50@gmail.com>
2025-09-18 13:55:30 +0000

    changes done

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -l
 
+# Get the HTML file argument
+HTML_FILE="$1"
+
+# Copy the provided HTML file to the Nginx directory
+cp "/github/workspace/$HTML_FILE" /usr/share/nginx/html/index.html
+
 # Start the Nginx server
 nginx -g "daemon off;"
